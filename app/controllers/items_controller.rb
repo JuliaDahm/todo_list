9019b8af6ca850a14f9ed6460ba2aa3812296de2
Julia Dahm <jukabrown@gmail.com>
2016-03-01 18:35:40 -0500

    final commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,46 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end  
+
+  def new
+    @items = Item.new 
+  end
+
+  def create 
+    @item = Item.new(item_params)
+
+    if @item.save
+     redirect_to items_path 
+    else
+      render :new 
+    end
+  end 
+
+  def update
+    @item = Item.find(params[:id])
+    if  @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
+  end 
+
+
+  def destroy
+   @item = Item.find(params[:id])
+   @item.destroy
+   redirect_to items_path
+ end 
+
+  def edit 
+    @item = Item.find(params[:id])
+  end 
+
+  private 
+
+  def item_params
+    params.require(:item).permit(:title)
+  end    
+
+end
